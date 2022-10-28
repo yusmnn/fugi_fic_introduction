5d8213acbe64a82b91594ddd1e610286c9961267
yusmnn <yusmanqq@gmail.com>
2022-10-29 03:42:44 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/fugi_fic_introduction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fugi_fic_introduction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Home Page",
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          "Introduction FUGI | flutter intensif club",
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
